36a0998493828cd7eed26f50597c9a0907890494
Relaxingmusic0474 <andywalls.s17@gmail.com>
2025-07-13 11:41:23 -0300

    Se ordenaron las funciones según el orden en que se implementan en collisions.c

--- incs/collisions.h
+++ incs/collisions.h
@@ -6,13 +6,13 @@
 #include "debug.h"
 
 bool hay_colision_con_bordes(Personaje*, Mapa);
-bool hay_colision_izquierda(Personaje*, Mapa);
-bool hay_colision_derecha(Personaje*, Mapa);
 bool hay_colision_superior(Personaje*, Mapa);
+bool hay_bloque_arriba(Personaje*, Mapa);
 bool hay_colision_inferior(Personaje*, Mapa);
 bool hay_bloque_debajo(Personaje*, Mapa);
-bool hay_bloque_arriba(Personaje*, Mapa);
+bool hay_colision_izquierda(Personaje*, Mapa);
 bool hay_bloque_izquierda(Personaje*, Mapa);
+bool hay_colision_derecha(Personaje*, Mapa);
 bool hay_bloque_derecha(Personaje*, Mapa);
 Procedure efectuar_colision(Personaje*, Mapa);
 
