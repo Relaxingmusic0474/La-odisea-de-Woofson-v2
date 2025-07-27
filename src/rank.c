19c83eb61d5b42ca6c0cbc575547033b323fcaca
Relaxingmusic0474 <andywalls.s17@gmail.com>
2025-07-27 18:51:58 -0300

    Se añadió una función para leer el ranking

--- src/rank.c
+++ src/rank.c
@@ -2,6 +2,7 @@
 
 bool leer_ranking(Ranking* ranking, Natural nro_nivel)
 {
+    Natural i;
     FILE* archivo_ranking = NULL;
     char nombre_archivo[LARGO] = {'\0'};
 
@@ -15,7 +16,14 @@ bool leer_ranking(Ranking* ranking, Natural nro_nivel)
         return false;
     }
 
-    
-    
+    i = 0;
 
+    while (i < MAX_DATOS && fscanf(archivo_ranking, "%s %hu", ranking->datos[i].nombre, &ranking->datos[i].puntaje) == 2)
+    {
+        ranking->datos[i].nro_nivel = nro_nivel;
+        i++;
+    }
+
+    fclose(archivo_ranking);
+    return true;
 }
