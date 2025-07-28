8418fc0337c6280a0e05f0392116d9e7fd28fa36
Relaxingmusic0474 <andywalls.s17@gmail.com>
2025-07-27 22:58:03 -0300

    Se añadió una función para la inserción en el ranking

--- incs/rank.h
+++ incs/rank.h
@@ -5,7 +5,11 @@
 #include "stdio.h"
 #include "types.h"
 #include "structs.h"
+#include "score.h"
 
-bool leer_ranking(Ranking*, Natural);
+bool leer_ranking(Ranking*, Natural);  // Lee el ranking de un cierto nivel
+bool modificar_ranking(Ranking*, Natural);  // Modifica el ranking de un cierto nivel
+Procedure insertar_en_ranking(Ranking*, char*, Natural);
+
 
 #endif // RANK_H
--- src/rank.c
+++ src/rank.c
@@ -12,7 +12,7 @@ bool leer_ranking(Ranking* ranking, Natural nro_nivel)
 
     if (!archivo_ranking)
     {
-        printf("Error al abrir el archivo del ranking del nivel %hu\n", nro_nivel);
+        printf("Error al abrir en modo de lectura el archivo del ranking del nivel %hu\n", nro_nivel);
         return false;
     }
 
@@ -26,4 +26,71 @@ bool leer_ranking(Ranking* ranking, Natural nro_nivel)
 
     fclose(archivo_ranking);
     return true;
+}
+
+
+bool modificar_ranking(Ranking* ranking, Natural nro_nivel)
+{
+    Natural i;
+    FILE* archivo_ranking = NULL;
+    char nombre_archivo[LARGO] = {'\0'};
+
+    sprintf(nombre_archivo, "assets/rankings/ranking-%hu.txt", nro_nivel);
+
+    archivo_ranking = fopen(nombre_archivo, "w");
+
+    if (!archivo_ranking)
+    {
+        printf("Error al abrir en modo de escritura el archivo del ranking del nivel %hu\n", nro_nivel);
+        return false;
+    }
+
+    for (i=0; i<MAX_DATOS; i++)
+    {
+        fprintf(archivo_ranking, "%s %hu\n", ranking->datos[i].nombre, ranking->datos[i].puntaje);
+    }
+
+    fclose(archivo_ranking);
+    return true;
+}
+
+
+Procedure intercambiar_datos(Datos* data_1, Datos* data_2)
+{
+    Datos aux;
+    aux = *data_1;
+    *data_1 = *data_2;
+    *data_2 = aux;
+}
+
+
+Procedure insertar_en_ranking(Ranking* ranking, char* nombre, Natural nivel)
+{
+    Natural i, j;
+    Datos data = {0};
+
+    if (puntuacion <= ranking->datos[MAX_DATOS-1].puntaje)  // Si no sobrepasa a la puntuación del último top 10, no entra en el ranking
+    {
+        return;
+    }
+
+    strcpy(data.nombre, nombre);
+    data.puntaje = puntuacion;
+    data.nro_nivel = nivel;
+
+    memset(ranking->datos[MAX_DATOS-1].nombre, '\0', sizeof(ranking->datos[MAX_DATOS-1].nombre));
+    ranking->datos[MAX_DATOS-1] = data;
+
+    for (i=MAX_DATOS-1; i>0; i--)
+    {
+        if (ranking->datos[i].puntaje < ranking->datos[i-1].puntaje)  // Puntuación es una variable global
+        {
+            intercambiar_datos(&ranking->datos[i], &ranking->datos[i-1]);
+        }
+        
+        else
+        {
+            break;
+        }
+    }
 }
